4fd60efe94cb3f4c79f2fed923437114e27c2ab3
ADMIN <ADMIN@LENGOCNHAN>
2022-12-12 10:05:02 +0700

    thanh toán

--- NNStore/Controllers/PaymentController.cs
+++ NNStore/Controllers/PaymentController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NNStore.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,41 @@ namespace NNStore.Controllers
 {
     public class PaymentController : Controller
     {
+        NNStoreEntities dbWeb = new NNStoreEntities();
         // GET: Payment
-        public ActionResult Index()
+        public ActionResult Payment()
         {
+            if (Session["UserName"] == null)
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            else
+            {
+                //Lấy infor từ giỏ hàng từ biến Sesstion
+                var lstCart = (List<CartModel>)Session["cart"];
+                //Gán dữ liệu cho order
+                Order objOrder = new Order();
+                objOrder.RecciverName = "Đơn hàng " + DateTime.Now.ToString("yyyyMMddHHmmss");
+                objOrder.UserId = int.Parse(Session["UserId"].ToString());
+                objOrder.CreatedAt = DateTime.Now;
+                objOrder.Status = 1;
+                dbWeb.Orders.Add(objOrder);
+                //Lưu infor dữu liệu vào bảng Order 
+                dbWeb.SaveChanges();
+                //Lấy OrderId vừa mới tạo để lưu vào bảng orderdetail.
+                int intOrderId = objOrder.Id;
+                List<OrderDetail> lstOrderDetail = new List<OrderDetail>();
+                foreach (var item in lstCart)
+                {
+                    OrderDetail obj = new OrderDetail();
+                    obj.Qty = item.Quantity;
+                    obj.OrderId = intOrderId;
+                    obj.ProductId = item.Product.Id;
+                    lstOrderDetail.Add(obj);
+                }
+                dbWeb.OrderDetails.AddRange(lstOrderDetail);
+                dbWeb.SaveChanges();
+            }
             return View();
         }
     }
